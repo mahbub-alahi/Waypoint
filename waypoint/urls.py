4c430baf3c1d12a7855da5b93f2a9d6133e4ca31
Mahbub Alahi <mahbub207@gmail.com>
2026-08-15 15:03:22 -0400

    Complete Week 12 ORM models admin and database catalog

--- waypoint/urls.py
+++ waypoint/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('report/', views.report, name='report'),
-    path('search/', views.search, name='search'),
-    path('catalog/', views.catalog, name='catalog'),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("report/", views.report, name="report"),
+    path("search/", views.search, name="search"),
+    path("catalog/", views.catalog, name="catalog"),
+    path("trails/", include("trails.urls")),
 ]
